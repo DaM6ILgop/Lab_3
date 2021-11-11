785582c935b576ad3965d13e21da1f7bcbd92fb9
DaM6ILgop <sanyaresnetnikov333@gmail.com>
2021-11-11 16:11:27 +0300

    7

---
+++ Задание_7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Задание_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibo(int n)
+            {
+                if (n == 1 || n == 2)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return fibo(n - 1) + fibo(n - 2);
+                }
+            }
+        point:
+            Console.WriteLine("Введите число для нахождения члена ряда Фибоначчи");
+            int f = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Под номером |{f}| стоит число -- |{fibo(f)}| из ряда Фибоначчи");
+            goto point;
+        }
+    }
+}
