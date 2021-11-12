a363e6aea0ea1498f8995b1e9a0c3fb311766b10
DaM6ILgop <sanyaresnetnikov333@gmail.com>
2021-11-12 15:35:00 +0300

    Create Program.cs

---
+++ Задание_6_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Задание_6_2
+{
+    class Program
+    { 
+
+        
+        static void Main(string[] args)
+        {
+            int recursion(int[] array) {
+                array = new int[10];
+                Random rand = new Random();
+
+               int recursion = 0;               
+                for (int i = 0; i<array.Length;++i) {
+                    array[i] = rand.Next(1, 10);
+                    recursion += array[i];
+                    Console.WriteLine(array[i]);
+                }              
+                return recursion;
+            }
+            int[] f = new int[0];          
+            Console.WriteLine($"Сумма элементов массива равна: {recursion(f)}");
+        }  
+    }
+}
