673b1c2f4e7d03bee0292bef936dfd38e8dcf7e8
DaM6ILgop <sanyaresnetnikov333@gmail.com>
2021-11-13 13:34:54 +0300

    Create Program.cs

---
+++ Задание_6_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Задание_6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 3, 5, 6, 8, 9, 9, 7, 9, 12, 45, 65 };
+
+            int minValue = array[0];
+            for (int i = 1; i < array.Length; i++) {
+                if (array[i] < minValue) {
+                    minValue = array[i];
+                }
+            }
+            Console.WriteLine($"Минимальное значение массива: {minValue}");
+        }
+    }
+}
