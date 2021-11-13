dc07e558df37b1bc76e1237cb3b50bf42edf4bf1
DaM6ILgop <sanyaresnetnikov333@gmail.com>
2021-11-13 15:50:02 +0300

    Create Program.cs

---
+++ Задание_6_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Задание_6_4
+{
+    class Program
+    {
+        static int MinValue(int[] Array, int i) {
+            if (i + 1 == Array.Length) {
+                return Array[i];
+            }
+            else {
+                return Math.Min(Array[i], MinValue(Array, ++i));
+            }
+        } 
+
+        static void Main(string[] args) {     
+            int[] Array = { 23,2435,4, 23, 43, 35, 72,19, 20, 23, 45, 6, 543, 987 };
+            int result = MinValue(Array,0);
+            Console.WriteLine($"Минимальное значение :{result}");        
+        }
+    }
+}
