9667dd745ebe5c556f8dcdebaf73557bd54f8e8a
DaM6ILgop <sanyaresnetnikov333@gmail.com>
2021-11-19 10:16:25 +0300

    Create Program.cs

---
+++ Второе_индивид/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Второе_индивид
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива\n масcив должен быть четной размерности");
+            int Razmer = int.Parse(Console.ReadLine());
+            int[] array = new int[100];
+            for (int i = 1; i < Razmer+1; i++) {
+                Console.WriteLine("Задайте значение массива ");
+                int w = int.Parse(Console.ReadLine());
+                array[i] = w;
+                
+                Console.WriteLine(array[i]);
+            }
+            for (int i = 1; i < Razmer+1; i++) {
+                
+                Console.Write(array[i] + "  ");  
+            }
+            int x;
+            int y;
+            int counter= 0;
+            int Razn;
+            int YslovieOut;
+            Console.WriteLine();
+            for (int i = 1; i < Razmer + 1; i++) {
+            
+                x = Math.Abs(array[i]);
+                y = Math.Abs(array[i-1]);
+                Razn = x - y;
+                YslovieOut = Math.Abs(Razn);
+                if (YslovieOut <= 5) {
+                    counter++;
+                }
+                else {
+                    Console.WriteLine("Числа вышли за предел");
+                }                          
+            }
+            Console.WriteLine("Результат ");
+            Console.WriteLine("|" +  counter + "|");
+        }
+    }
+}
+//Math.Abs(-5);
